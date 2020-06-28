2c31e736f5fd47f3529ddca913440dd3a687e461
EMIRATES\S924041 <krishnachaitanya.kolluri@dnata.com>
2020-06-28 14:45:54 +0400

    Updates

---
+++ precache-manifest.3ff88a1d95fa882dfae61b82070bfcc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23cdaf7bc1bb517ccb66",
+    "url": "/me/static/css/main.5cb26caf.chunk.css"
+  },
+  {
+    "revision": "23cdaf7bc1bb517ccb66",
+    "url": "/me/static/js/main.23cdaf7b.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "973b6d853800d7c4ed8efe702adf2569",
+    "url": "/me/index.html"
+  }
+];
